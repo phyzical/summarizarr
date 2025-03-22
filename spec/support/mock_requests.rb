cd22592d0f33fe9cb844cd8b8427ffbc213cb33c
phyzical <5182053+phyzical@users.noreply.github.com>
2025-03-22 16:18:51 +0800

    move mock requests into their own folder

--- spec/support/mock_requests.rb
+++ spec/support/mock_requests.rb
@@ -3,7 +3,14 @@
 module MockRequests
   def stub_endpoint(url)
     url = URI(url)
-    stub_request(:get, /#{url}/).to_return(status: 200, body: File.read("spec/support/#{url.host}#{url.path}.json"))
+    stub_request(:get, /#{url}/).to_return(
+      status: 200,
+      body: File.read("spec/support/requests/#{url.host}#{url.path}.json")
+    )
+  end
+
+  def stub_fakeserver
+    stub_request(:get, /fakeserver/).to_return(status: 500)
   end
 
   def stub_sonarr
@@ -21,5 +28,6 @@ module MockRequests
   def stub_all
     stub_sonarr
     stub_radarr
+    stub_fakeserver
   end
 end
