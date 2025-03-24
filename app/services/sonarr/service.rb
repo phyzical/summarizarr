68c99275d8368be43e1f089a3889bd077d1833d5
phyzical <5182053+phyzical@users.noreply.github.com>
2025-03-24 20:31:49 +0800

    refactor services to not need summary  item, so merge in service

--- app/services/sonarr/service.rb
+++ app/services/sonarr/service.rb
@@ -2,30 +2,47 @@
 
 module Sonarr
   class Service < BaseService
+    API_PREFIX = '/api/v3'
     # curl -X 'GET' 'http://sonarr:8989/api/v3/history/since?date=2025-03-10&includeEpisode=true&includeSeries=true&apikey=asd'   -H 'accept: application/json' # rubocop:disable Layout/LineLength
-    SINCE_ENDPOINT = '/api/v3/history/since'
+    SINCE_ENDPOINT = "#{API_PREFIX}/history/since".freeze
     # curl -X 'GET' 'http://sonarr:8989/api/v3/system/status?apikey=asd'   -H 'accept: application/json'
-    STATUS_ENDPOINT = '/api/v3/system/status'
+    STATUS_ENDPOINT = "#{API_PREFIX}/system/status".freeze
 
     def items
       @items ||=
-        Request
-          .perform(
-            url: "#{base_url}#{SINCE_ENDPOINT}",
-            api_key:,
-            get_vars: {
-              date: from_date,
-              includeEpisode: true,
-              includeSeries: true
-            }
-          )
-          .map { |json| Item.from_json(json:) }
-          .filter { |item| Item::EVENT_TYPES.value?(item.event_type) }
-          .group_by(&:title)
-          .map { |title, items| Summary::Item.new(title:, items:) }
+        combine_items(
+          Request
+            .perform(
+              url: "#{base_url}#{SINCE_ENDPOINT}",
+              headers:,
+              get_vars: {
+                date: from_date,
+                includeEpisode: true,
+                includeSeries: true
+              }
+            )
+            .map { |json| Item.from_json(json:) }
+            .filter { |item| Item::EVENT_TYPES.value?(item.event_type) }
+        )
     end
 
     private
+
+    def combine_items(combinable_items)
+      combinable_items
+        .group_by(&:title)
+        .values
+        .map do |items|
+          old = items.find(&:deletion?)
+          new = items.reject(&:deletion?).first
+          new.old_quality = old.quality if old
+          new
+        end
+    end
+
+    def headers
+      { Authorization: "Bearer #{api_key}" }
+    end
 
     def app_name
       'Sonarr'
@@ -36,7 +53,7 @@ module Sonarr
     end
 
     def pull_app_name
-      Request.perform(url: "#{base_url}#{STATUS_ENDPOINT}", api_key:)[:appName]
+      Request.perform(url: "#{base_url}#{STATUS_ENDPOINT}", headers:)[:appName]
     end
   end
 end
--- spec/services/sonarr/service_spec.rb
+++ spec/services/sonarr/service_spec.rb
@@ -2,49 +2,51 @@
 
 require 'spec_helper'
 
-RSpec.describe Sonarr::Service do
-  include MockRequests
-  subject(:items) { service.items }
+module Sonarr
+  RSpec.describe Service do
+    include MockRequests
+    subject(:items) { service.items }
 
-  let(:service) { described_class.new }
+    let(:service) { described_class.new }
 
-  before { stub_sonarr }
+    before { stub_sonarr }
 
-  it 'returns history only containing expected types and groups by title' do
-    expect(items.count).to be(222)
-    expect(items).to all(be_a(Summary::Item::Thing))
-  end
-
-  context 'when apps required config is missing' do
-    before { allow(Config.get).to receive(:sonarr).and_return(Config::AppConfig.new(**config)) }
-
-    let(:config) { { api_key:, base_url: } }
-    let(:base_url) { Faker::Internet.url }
-    let(:api_key) { Faker::Internet.password }
-
-    context 'when api_key missing' do
-      let(:api_key) { '' }
-
-      it 'alerts and skips' do
-        expect { items }.to output(/Sonarr API Key is not set, will be skipped/).to_stdout
-      end
+    it 'returns history only containing expected types and groups by title' do
+      expect(items.count).to be(222)
+      expect(items).to all(be_a(Item::Thing))
     end
 
-    context 'when base_url missing' do
-      let(:base_url) { '' }
+    context 'when apps required config is missing' do
+      before { allow(Config.get).to receive(:sonarr).and_return(Config::AppConfig.new(**config)) }
 
-      it 'alerts and skips' do
-        expect { items }.to output(/Sonarr URL is not set, will be skipped/).to_stdout
+      let(:config) { { api_key:, base_url: } }
+      let(:base_url) { Faker::Internet.url }
+      let(:api_key) { Faker::Internet.password }
+
+      context 'when api_key missing' do
+        let(:api_key) { '' }
+
+        it 'alerts and skips' do
+          expect { items }.to output(/Sonarr API Key is not set, will be skipped/).to_stdout
+        end
       end
-    end
 
-    context 'when the wrong / invalid app is found given config' do
-      before { stub_fakeserver }
+      context 'when base_url missing' do
+        let(:base_url) { '' }
 
-      let(:base_url) { 'http://fakeserver' }
+        it 'alerts and skips' do
+          expect { items }.to output(/Sonarr URL is not set, will be skipped/).to_stdout
+        end
+      end
 
-      it 'alerts and skips' do
-        expect { items }.to output(/Error this is not an instance of Sonarr/).to_stdout
+      context 'when the wrong / invalid app is found given config' do
+        before { stub_fakeserver }
+
+        let(:base_url) { 'http://fakeserver' }
+
+        it 'alerts and skips' do
+          expect { items }.to output(/Error this is not an instance of Sonarr/).to_stdout
+        end
       end
     end
   end
