3b45baec3146bc7d63ce72adaa4bc5ce82c0308e
phyzical <5182053+phyzical@users.noreply.github.com>
2025-03-30 23:13:35 +0800

    refactor main into a service

--- spec/services/main_spec.rb
+++ spec/services/main_spec.rb
@@ -5,11 +5,19 @@ require 'spec_helper'
 RSpec.describe Main do
   include MockRequests
 
-  subject(:run) { described_class.run }
+  subject(:run) { described_class.new.run }
 
-  before { stub_all }
+  before do
+    stub_all
+    allow_any_instance_of(described_class).to receive(:sleep).and_wrap_original do |_method, *_args| # rubocop:disable RSpec/AnyInstance
+      raise 'TestExit'
+    end
+  end
+
+  let(:sleep_time) { 60 * 60 * 24 * 7 } # 7 days of seconds
 
   it 'runs' do
-    expect(run).to include('Sonarr Summary', 'Radarr Summary')
+    # TODO: add more tests to make sure on rerun we are a week later
+    expect { run }.to output(/Sleeping for #{sleep_time}.0 seconds/).to_stdout.and raise_error('TestExit')
   end
 end
