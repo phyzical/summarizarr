70d6e4a837f4d6f14ae908816aef72cd31bcaabb
phyzical <5182053+phyzical@users.noreply.github.com>
2025-03-30 19:01:41 +0800

    add logic to allow a notification service to support which output to use

---
+++ app/services/notifications/discord.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Notifications
+  module Discord
+    class << self
+      def notify(contents:)
+        pp contents
+      end
+    end
+  end
+end
---
+++ app/services/notifications/notification.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Notifications
+  module Notification
+    class << self
+      def notify(contents:)
+        discord.enabled? ? Discord::Notification.notify(contents:) : Generic::Notification.notify(contents:)
+      end
+
+      private
+
+      delegate :discord, to: :config
+
+      def config
+        @config ||= Config.get
+      end
+    end
+  end
+end
