1ea99c2546221f15e46e228daac16c9e8c84f9d5
phyzical <5182053+phyzical@users.noreply.github.com>
2025-03-30 23:13:13 +0800

    fix up the discord post request logic

--- app/services/notifications/discord.rb
+++ app/services/notifications/discord.rb
@@ -2,40 +2,43 @@
 
 module Notifications
   #  TODO: Should these be models?
-  class Discord
-    def notify(contents:)
-      Request.post(
-        url: discord.webhook_url,
-        headers: {
-          'Content-Type' => 'application/json'
-        },
-        body: {
-          content: contents,
-          username: discord.username,
-          avatar_url: discord.avatar_url
-          # embeds: [
-          #   {
-          #     title: contents[:title],
-          #     description: contents[:description],
-          #     color: contents[:color],
-          #     fields: contents[:fields],
-          #     footer: {
-          #       text: contents[:footer_text],
-          #       icon_url: contents[:footer_icon_url]
-          #     },
-          #     timestamp: Time.now.utc.iso8601
-          #   }
-          # ]
-        }.to_json
-      )
-    end
+  module Discord
+    class << self
+      def notify(contents:)
+        Request.perform(
+          type: :post,
+          url: discord.webhook_url,
+          headers: {
+            'Content-Type' => 'application/json'
+          },
+          body: {
+            content: contents,
+            username: discord.username,
+            avatar_url: discord.avatar_url
+            # embeds: [
+            #   {
+            #     title: contents[:title],
+            #     description: contents[:description],
+            #     color: contents[:color],
+            #     fields: contents[:fields],
+            #     footer: {
+            #       text: contents[:footer_text],
+            #       icon_url: contents[:footer_icon_url]
+            #     },
+            #     timestamp: Time.now.utc.iso8601
+            #   }
+            # ]
+          }.to_json
+        )
+      end
 
-    private
+      private
 
-    delegate :discord, to: :config
+      delegate :discord, to: :config
 
-    def config
-      @config ||= Config.get
+      def config
+        @config ||= Config.get
+      end
     end
   end
 end
