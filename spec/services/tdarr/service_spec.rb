968b75c5d92b04f9350d1333130722ccd7392682
phyzical <5182053+phyzical@users.noreply.github.com>
2025-04-01 19:43:42 +0800

    add tdarr service

---
+++ spec/services/tdarr/service_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+module Tdarr
+  RSpec.describe Service do
+    include MockRequests
+    subject(:items) { service.items }
+
+    let(:service) { described_class.new }
+
+    before { stub_tdarr }
+
+    it 'returns history only containing expected types and groups by title' do
+      expect(items.count).to be(60)
+      expect(items).to all(be_a(Item::Thing))
+    end
+
+    context 'when apps required config is missing' do
+      before { allow(Config.get).to receive(:tdarr).and_return(Config::AppConfig.new(**config)) }
+
+      let(:config) { { api_key:, base_url: } }
+      let(:base_url) { Faker::Internet.url }
+      let(:api_key) { Faker::Internet.password }
+
+      context 'when base_url missing' do
+        let(:base_url) { '' }
+
+        it 'alerts and skips' do
+          expect { items }.to output(/Tdarr URL is not set, will be skipped/).to_stdout
+        end
+      end
+
+      context 'when the wrong / invalid app is found given config' do
+        before { stub_fakeserver }
+
+        let(:base_url) { 'http://fakeserver' }
+
+        it 'alerts and skips' do
+          expect { items }.to output(/Error this is not an instance of Tdarr/).to_stdout
+        end
+      end
+    end
+  end
+end
