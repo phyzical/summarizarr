8bce4d95a021a3c4d0b042b61ac831f02d595819
phyzical <5182053+phyzical@users.noreply.github.com>
2025-04-15 21:56:04 +0800

    improve output of body for requests

--- app/models/request.rb
+++ app/models/request.rb
@@ -8,10 +8,14 @@ module Request
   Thing =
     Struct.new(:type, :url, :headers, :get_vars, :body) do
       def perform
-        puts "Requesting #{http_request.uri}"
-        response = http.request(http_request)
+        request = http_request
+        puts "Requesting #{request.uri}"
+        pp body if request.body
+        response = http.request(request)
         if response.code == '200' && response.body != ''
           return JSON.parse(response.body.force_encoding('UTF-8'), symbolize_names: true)
+        elsif response.code == '400'
+          raise "Error: #{response.code} - #{response.message}"
         end
         {}
       end
@@ -24,13 +28,13 @@ module Request
         @http_request = net_type.new(URI("#{url}#{uris ? "?#{uris}" : ''}"), **headers)
         @http_request['Content-Type'] = 'application/json'
         @http_request['Accept'] = 'application/json'
-        @http_request.body = body if body != {}
+        @http_request.body = body.to_json if body != {}
         @http_request
       end
 
       def http
         https = Net::HTTP.new(http_request.uri.host, http_request.uri.port)
-        https.use_ssl = http_request.uri.host.include?('https')
+        https.use_ssl = http_request.uri.scheme == 'https'
         https
       end
 
