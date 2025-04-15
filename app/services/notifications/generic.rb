f4e4f96dd93453b7d63b34c29f3b53fe6c44e453
phyzical <5182053+phyzical@users.noreply.github.com>
2025-04-15 21:59:40 +0800

    update generic notification to output the summary app name and each group and group items

--- app/services/notifications/generic.rb
+++ app/services/notifications/generic.rb
@@ -3,8 +3,17 @@
 module Notifications
   module Generic
     class << self
-      def notify(contents:)
-        pp contents
+      SEPARATOR =
+        "\n----------------------------------------------------------------------------------------------------\n"
+      DOT_POINT = "\n* "
+
+      def notify(service:)
+        pp "#{SEPARATOR}#{service.app_name}"
+        pp service.summary
+        service.grouped_items.each do |date, date_items|
+          pp "#{SEPARATOR}#{date}"
+          date_items.each { |item| pp "#{DOT_POINT} #{item.summary}" }
+        end
       end
     end
   end
