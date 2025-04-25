ae0ed8e52f0bce4956149d3fe1b7e50b011160e4
phyzical <5182053+phyzical@users.noreply.github.com>
2025-04-25 15:21:28 +0800

    rtefactor items into its on group

--- spec/services/readarr/service_spec.rb
+++ spec/services/readarr/service_spec.rb
@@ -5,39 +5,42 @@ require 'spec_helper'
 module Readarr
   RSpec.describe Service do
     include MockRequests
-    subject(:items) { service.items }
 
     let(:service) { described_class.new }
 
     before { stub_readarr }
 
-    it 'returns history only containing expected types and groups by title' do
-      expect(items.count).to be(2)
-      expect(items).to all(be_a(Item::Thing))
-    end
+    describe '#items' do
+      subject(:items) { service.items }
 
-    context 'when apps required config is missing' do
-      before { allow(Config.get).to receive(:readarr).and_return(Config::AppConfig.new(**config)) }
-
-      let(:config) { { api_key:, base_url: } }
-      let(:base_url) { Faker::Internet.url }
-      let(:api_key) { Faker::Internet.password }
-
-      context 'when base_url missing' do
-        let(:base_url) { '' }
-
-        it 'alerts and skips' do
-          expect { items }.to output(/Readarr URL is not set, will be skipped/).to_stdout
-        end
+      it 'returns history only containing expected types and groups by title' do
+        expect(items.count).to be(2)
+        expect(items).to all(be_a(Item::Thing))
       end
 
-      context 'when the wrong / invalid app is found given config' do
-        before { stub_fakeserver }
+      context 'when apps required config is missing' do
+        before { allow(Config.get).to receive(:readarr).and_return(Config::AppConfig.new(**config)) }
 
-        let(:base_url) { 'http://fakeserver' }
+        let(:config) { { api_key:, base_url: } }
+        let(:base_url) { Faker::Internet.url }
+        let(:api_key) { Faker::Internet.password }
 
-        it 'alerts and skips' do
-          expect { items }.to output(/Error this is not an instance of Readarr/).to_stdout
+        context 'when base_url missing' do
+          let(:base_url) { '' }
+
+          it 'alerts and skips' do
+            expect { items }.to output(/Readarr URL is not set, will be skipped/).to_stdout
+          end
+        end
+
+        context 'when the wrong / invalid app is found given config' do
+          before { stub_fakeserver }
+
+          let(:base_url) { 'http://fakeserver' }
+
+          it 'alerts and skips' do
+            expect { items }.to output(/Error this is not an instance of Readarr/).to_stdout
+          end
         end
       end
     end
