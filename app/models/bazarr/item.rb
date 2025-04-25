a4020ccfb9e3f5397e0a0c483c70d87ac4196ee5
phyzical <5182053+phyzical@users.noreply.github.com>
2025-04-25 12:52:54 +0800

    update mylar with comic and issue number props

--- app/models/bazarr/item.rb
+++ app/models/bazarr/item.rb
@@ -8,7 +8,7 @@ module Bazarr
       episode: :episode,
       season: :season,
       description: :description,
-      date: :date,
+      parsed_timestamp: :date,
       score: :score,
       title: :title
     }.freeze
@@ -21,7 +21,7 @@ module Bazarr
         json[:episode] = splits.last.to_i
         json[:season] = splits.first.to_i
       end
-      json[:date] = DateTime.strptime(json[:parsed_timestamp], '%m/%d/%y %H:%M:%S').to_date
+      json[:parsed_timestamp] = DateTime.strptime(json[:parsed_timestamp], '%m/%d/%y %H:%M:%S').to_date
       Thing.new(**json.slice(*ATTRIBUTES.keys).transform_keys { |k| ATTRIBUTES[k] })
     end
 
--- spec/factories/mylar3/item.rb
+++ spec/factories/mylar3/item.rb
@@ -3,7 +3,8 @@
 FactoryBot.define do
   factory :mylar3_item, class: Mylar3::Item::Thing.name do
     event_type { Mylar3::Item::EVENT_TYPES.values.sample }
-    title { "#{Faker::Book.title} #{Faker::Number.number}" }
+    comic { Faker::Book.title }
+    issue { Faker::Number.number(digits: 2) }
     deletion? { Faker::Boolean.boolean }
     date { Faker::Date.between(from: 2.days.ago, to: Date.today) }
   end
