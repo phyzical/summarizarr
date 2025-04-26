7432e340dcc8f25bfdea23a756e45c5387c1c0fc
phyzical <5182053+phyzical@users.noreply.github.com>
2025-04-26 14:43:47 +0800

    update bazarr spec

--- spec/models/bazarr/item_spec.rb
+++ spec/models/bazarr/item_spec.rb
@@ -12,7 +12,10 @@ module Bazarr
       context 'when item is episode' do
         let(:json) do
           JSON.parse(
-            File.read("spec/support/requests/bazarr#{Service.episode_history_endpoint}.json", encoding: 'bom|utf-8'),
+            File.read(
+              "spec/support/requests/bazarr#{Service.episode_history_endpoint}?start=0.json",
+              encoding: 'bom|utf-8'
+            ),
             symbolize_names: true
           )[
             :data
@@ -22,13 +25,13 @@ module Bazarr
         it 'runs' do
           expect(from_json.to_h).to match(
             {
-              series: 'Penn & Teller: Fool Us',
-              date: Date.parse('29/03/2025'),
+              series: 'Keeping Up with the Kardashians',
+              date: 'Sat, 26 Apr 2025'.to_date,
               language: 'English',
-              episode: 10,
-              season: 11,
-              title: 'Gotcha!',
-              description: 'English subtitles downloaded from whisperai with a score of 66.94%.',
+              episode: 18,
+              season: 8,
+              title: 'All Signs Point to North',
+              description: 'English HI subtitles downloaded from whisperai with a score of 66.94%.',
               score: '66.94%'
             }
           )
@@ -38,7 +41,10 @@ module Bazarr
       context 'when item is movie' do
         let(:json) do
           JSON.parse(
-            File.read("spec/support/requests/bazarr#{Service.movie_history_endpoint}.json", encoding: 'bom|utf-8'),
+            File.read(
+              "spec/support/requests/bazarr#{Service.movie_history_endpoint}?start=0.json",
+              encoding: 'bom|utf-8'
+            ),
             symbolize_names: true
           )[
             :data
@@ -52,10 +58,10 @@ module Bazarr
               language: 'English',
               episode: nil,
               season: nil,
-              date: Date.parse('29/03/2025'),
-              title: 'Back in Action',
-              description: 'English HI subtitles downloaded from opensubtitlescom with a score of 85.0%.',
-              score: '85.0%'
+              date: 'Fri, 25 Apr 2025'.to_date,
+              title: 'Companion',
+              description: 'English subtitles downloaded from opensubtitlescom with a score of 85.83%.',
+              score: '85.83%'
             }
           )
         end
