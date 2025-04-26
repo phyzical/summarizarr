e0e356fefbcd5d2c2954c859c2f6b7d0c20bee59
phyzical <5182053+phyzical@users.noreply.github.com>
2025-04-26 14:46:08 +0800

    rework the generic noticiation with the new groouping

--- app/services/notifications/generic.rb
+++ app/services/notifications/generic.rb
@@ -5,14 +5,20 @@ module Notifications
     class << self
       SEPARATOR =
         "\n----------------------------------------------------------------------------------------------------\n"
-      DOT_POINT = "\n* "
+      DOT_POINT = '* '
 
       def notify(service:)
-        pp "#{SEPARATOR}#{service.app_name}"
-        pp service.summary
-        service.grouped_items.each do |date, date_items|
-          pp "#{SEPARATOR}#{date}"
-          date_items.each { |item| pp "#{DOT_POINT} #{item.summary}" }
+        puts "#{SEPARATOR}#{service.class::APP_NAME}"
+        puts service.summary
+        service.grouped_items.each do |primary_group, primary_group_items|
+          puts "#{SEPARATOR}#{service.class::PRIMARY_GROUP_CONTEXT}: #{primary_group}"
+          primary_group_items.each do |secondary_group, secondary_group_items|
+            puts "#{service.class::SECONDARY_GROUP_CONTEXT}: #{secondary_group}"
+            secondary_group_items.each do |fallback_group, fallback_group_items|
+              puts "#{service.class::FALLBACK_GROUP_CONTEXT}: #{fallback_group}"
+              fallback_group_items.each { |item| puts "#{DOT_POINT}#{item.summary}" }
+            end
+          end
         end
       end
     end
