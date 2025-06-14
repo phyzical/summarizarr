6e9ac3a332c36ecc2464c72cfa347f29d992b569
phyzical <5182053+phyzical@users.noreply.github.com>
2025-06-14 14:59:14 +0800

    adjust the main entrypoint

--- main.rb
+++ main.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
-Dir[File.join(__dir__, 'app', '**', '**', '*.rb')].each do |file|
-  require_relative file unless file.include?('summary/service')
-end
-# must be loaded last
-require_relative 'app/services/summary/service'
+require_relative 'app/services/main'
 
 # :nocov:
 Main.new.run if __FILE__ == $PROGRAM_NAME
