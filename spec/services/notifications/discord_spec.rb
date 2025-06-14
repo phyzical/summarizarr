fe4305ab73b6cf0228ba2e6dc7bf5b5a68d80282
phyzical <5182053+phyzical@users.noreply.github.com>
2025-06-14 15:00:04 +0800

    finish the last spec

--- spec/services/notifications/discord_spec.rb
+++ spec/services/notifications/discord_spec.rb
@@ -6,25 +6,14 @@ module Notifications
   RSpec.describe Discord do
     include MockRequests
 
-    before do
-      allow(Config.get).to receive(:discord).and_return(Config::NotificationConfig.new(**config))
-      stub_request(:post, 'http://fakediscord:443/link/12345').with(
-        body: {
-          content: anything,
-          embeds: anything,
-          username: 'username',
-          avatar_url: 'https://fakediscord/avatar/12345'
-        }.to_json,
-        headers: {
-          'Accept' => 'application/json',
-          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'Content-Type' => 'application/json',
-          'Host' => 'fakediscord',
-          'User-Agent' => 'Ruby'
-        }
-      ).to_return(status: 200, body: '', headers: {})
-      stub_sonarr
-      allow(Sonarr::Service).to receive(:new).and_call_original
+    let(:headers) do
+      {
+        Accept: 'application/json',
+        'Accept-Encoding': 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'Content-Type': 'application/json',
+        Host: 'fakediscord',
+        'User-Agent': 'Ruby'
+      }
     end
 
     let(:service) { Sonarr::Service.new }
@@ -38,11 +27,38 @@ module Notifications
       }
     end
 
+    before do
+      allow(Config.get).to receive(:discord).and_return(Config::NotificationConfig.new(**config))
+      stub_sonarr
+      allow(Sonarr::Service).to receive(:new).and_call_original
+      stub_request(:post, config[:webhook_url]).with(
+        body: {
+          content: '',
+          username: config[:username],
+          avatar_url: config[:avatar_url],
+          embeds: anything
+        },
+        headers:
+      ).to_return(status: 200, body: '', headers: {})
+      allow(Request).to receive(:perform).and_call_original
+    end
+
     describe '.notify' do
       subject(:notify) { described_class.notify(service:) }
 
-      it 'prints the contents' do
-        expect { notify }.to output(%r{Requesting https://fakediscord/link/12345}).to_stdout
+      let(:expected_count) do
+        #once for summary, once for each series + season
+        i = 1
+        service.grouped_items.each { |_, primaries| primaries.each { |_, secondaries| i += secondaries.count } }
+        i
+      end
+
+      it 'notifies the summary, one for each primary + secondary combo' do
+        notify
+        expect(Request).to have_received(:perform)
+          .with(url: config[:webhook_url], type: :post, headers: anything, body: anything)
+          .exactly(expected_count)
+          .times
       end
     end
   end
