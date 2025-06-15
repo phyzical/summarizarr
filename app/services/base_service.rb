59937833e1ce88ce6cd1bc92088e23f4b9ae53c8
phyzical <5182053+phyzical@users.noreply.github.com>
2025-06-15 14:49:13 +0800

    implement logic to filter out items at the noticiaton level (grouped)

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -19,14 +19,17 @@ class BaseService
 
   def grouped_items
     @grouped_items ||=
-      sort_and_group(items, self.class::PRIMARY_GROUP_CONTEXT).transform_values do |primary_groups|
+      sort_and_group(
+        notification_filter(items:),
+        self.class::PRIMARY_GROUP_CONTEXT
+      ).transform_values do |primary_groups|
         sort_and_group(primary_groups, self.class::SECONDARY_GROUP_CONTEXT).transform_values do |secondary_groups|
           secondary_groups.sort_by { |item| item[self.class::ITEM_SORT_CONTEXT] || '' }
         end
       end
   end
 
-  delegate :from_date, to: :config
+  delegate :from_date, :notify_upgraded_items?, to: :config
   delegate :base_url, :api_key, :extra_info?, to: :app_config
 
   def items
@@ -41,20 +44,25 @@ class BaseService
 
   private
 
-  def process # rubocop:disable Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
+  def process
     page = 1
     items = []
     loop do
-      pulls = pulls(page:)
-      # :nocov:
-      break if pulls.all?(&:empty?)
-      # :nocov:
-      pulls = pulls.map { |pull| pull.map { |json| map(json:) } }
+      pulls = pulls(page:).map { |pull| pull.map { |json| map(json:) } }
       items += pulls.flatten
       page += 1
-      break if pulls.all? { |pull| pull.any? { |item| item.date < from_date } }
+      break if last_page?(pulls:)
     end
     items.filter { |item| item.date >= from_date && filter(item:) }
+  end
+
+  def last_page?(pulls:)
+    pulls.any? { |pull| pull.any? { |item| item.date < from_date } }
+  end
+
+  def notification_filter(items:)
+    return items if notify_upgraded_items?
+    items.reject(&:upgrade?)
   end
 
   # :nocov:
